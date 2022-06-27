1c4eb9927b315baaa68e8d4417e24b3fd314213f
Vika333 <70606811+Vika333@users.noreply.github.com>
2022-06-27 15:51:21 +0300

    Add files via upload

---
+++ ModuleC/matem.h
@@ -0,0 +1,13 @@
+#include "math.h"
+
+double _k(double a, double b, double c, double d);
+
+double _pk(double n);
+
+double _d(double a, double b, double c);
+
+double _lnv(double n, double k);
+
+double _ntp(double a, double b);
+
+int _summ(int n, int k);
